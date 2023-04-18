0d3356f530f7643892e4364b7f69e780dc2c60aa
Muxammedali <77286466+Muxammedali@users.noreply.github.com>
2023-04-18 14:53:40 +0400

    Update 1.js

--- 0 Arrays/1.js
+++ 0 Arrays/1.js
@@ -2,7 +2,7 @@ var count = ["Europe" , "Asia" , "Afrika"] ,
 population = [142 , 100 , 58];
 
 var elements = document.getElementsByTagName('div')[0],
-br = "milions<br><br>",
+br = "milions<br><br>", //for indentation(Space) 
 eu = count[0] + " - "+ population[0]+br, 
 as = count[1] + " - "+ population[1]+br,
 af = count[2] + " - "+ population[2];
